aee7972ed21bea45f2e47ee0e5df334362c80f24
Davide Amadei <davide.amadei97@gmail.com>
2024-05-09 13:18:34 +0200

    first push

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # Multimodal-Sentiment-Analysis
+
+Thesis for Master Degree in Computer Science AI curriculum at University of Pisa.<br>
+
+Sentiment analysis on text-image(s) pairs extracted from tweets.<br>
+
+The dataset was created by Roberto Cannarella for his thesis.
---
+++ download_dataset.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+download_file(){
+    fileid="$1"
+    filename="$2"
+    curl -C - -L "https://drive.usercontent.google.com/download?id=${fileid}&export=download&confirm=t" -o "${filename}"    
+}
+
+mkdir dataset
+
+download_file 1x5zOBcS2ktknP_lDTdLfMYMzMAqc_xMU dataset/merged_df_with_gold.pkl
+download_file 1YyFuWGQGa_TK05PBX36PXgssurcC6oy_ dataset/merged_df_with_gold_freq1.pkl
+download_file 1g93tl93ZbO2BIrBKALwLhnxNhDIaDjeR dataset/df_no_duplicated_with_path2.pkl
+download_file 1A13z757xlZzht6JGFnsC5tp1RTvAfZmT dataset/df_duplicated_with_path.pkl
+
+download_file 1gfce4Ko3GsE4eJ2ILtr5swQoRXMRyFpS dataset/images.zip
