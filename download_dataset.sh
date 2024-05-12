f55d84fa6c684cab2ee89d27b4730ad324bcd618
Davide Amadei <davide.amadei97@gmail.com>
2024-05-12 20:20:30 +0200

    script to download dataset and early analysis and clean up of dataset

--- download_dataset.sh
+++ download_dataset.sh
@@ -5,11 +5,11 @@ download_file(){
     curl -C - -L "https://drive.usercontent.google.com/download?id=${fileid}&export=download&confirm=t" -o "${filename}"    
 }
 
-mkdir dataset
+mkdir -p dataset/raw
 
-download_file 1x5zOBcS2ktknP_lDTdLfMYMzMAqc_xMU dataset/merged_df_with_gold.pkl
-download_file 1YyFuWGQGa_TK05PBX36PXgssurcC6oy_ dataset/merged_df_with_gold_freq1.pkl
-download_file 1g93tl93ZbO2BIrBKALwLhnxNhDIaDjeR dataset/df_no_duplicated_with_path2.pkl
-download_file 1A13z757xlZzht6JGFnsC5tp1RTvAfZmT dataset/df_duplicated_with_path.pkl
+download_file 1x5zOBcS2ktknP_lDTdLfMYMzMAqc_xMU dataset/raw/merged_df_with_gold.pkl
+download_file 1YyFuWGQGa_TK05PBX36PXgssurcC6oy_ dataset/raw/merged_df_with_gold_freq1.pkl
+download_file 1g93tl93ZbO2BIrBKALwLhnxNhDIaDjeR dataset/raw/df_no_duplicated_with_path2.pkl
+download_file 1A13z757xlZzht6JGFnsC5tp1RTvAfZmT dataset/raw/df_duplicated_with_path.pkl
 
-download_file 1gfce4Ko3GsE4eJ2ILtr5swQoRXMRyFpS dataset/images.zip
+download_file 1gfce4Ko3GsE4eJ2ILtr5swQoRXMRyFpS dataset/raw/images.zip
