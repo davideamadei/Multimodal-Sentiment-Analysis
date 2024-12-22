76328fe26dd149ab02178685d049569a8b70a018
davideamadei <davide.amadei97@gmail.com>
2024-12-22 18:03:40 +0100

    imrpoved overall structure of code for zero shot LLaVA script

--- zero_shot_LLaVA.py
+++ zero_shot_LLaVA.py
@@ -34,116 +34,95 @@ if __name__ == "__main__":
     mode="M"
     test_data, _ = MulTweEmoDataset.load(csv_path="./dataset/test_MulTweEmo.csv", mode=mode, drop_something_else=True, force_override=True, test_split=None, seed=123)
 
-#    index = 0
-#    train = train.iloc[index:index+1]
+    predictions = np.zeros(len(test_data), len(labels))
+
     if not args.binary_prediction:
         prompt_format = lambda text: f"The image is paired with this text: \"{text}\". Considering both image and text, choose which emotions are most elicited among this list: {labels}. Answer with only the list of chosen emotions."
-        prompts = []
 
-        for i, row in test_data.iterrows():
-            conversation = [
-                {
-                    "role": "user",
-                    "content": [
-                        {"type": "image"},
-                        {"type": "text", "text": prompt_format(row["tweet"])},
-                        ],
-                },
-            ]
-            prompts.append(processor.apply_chat_template(conversation, add_generation_prompt=True))
-        processed_images= []
-        for img in test_data["img_path"]:
-            if isinstance(img, str):
-                if img.startswith('http'):
-                    response = requests.get(img)
-                    image = Image.open(BytesIO(response.content)).convert('RGB')
-                else:
-                    image = Image.open(img).convert('RGB')
-            elif isinstance(img, Image.Image):
-                image = img.convert('RGB')
-            else:
-                raise ValueError("Unsupported image format")
-            processed_images.append(image)
-        inputs = []
-        for i in range(len(prompts)):
-            inputs.append(processor(images=processed_images[i], text=prompts[i], return_tensors="pt", padding=True).to(device))
         outputs = []
         with torch.no_grad():
-            for i in range(len(inputs)):
-        #       for i in range(1):
-                    generate_ids = model.generate(**(inputs[i]))
-        #           generate_ids = model.generate(input_ids=inputs["input_ids"][i:i+1], attention_mask=inputs["attention_mask"][i:i+1], pixel_values=inputs["pixel_values"][i:i+1], image_sizes=inputs["image_sizes"][i:i+1])
-                    outputs.append(processor.decode(generate_ids[0, inputs[i]["input_ids"].shape[1]:], skip_special_tokens=True))
-        # Generate
-    #       generate_ids = model.generate(**inputs, max_new_tokens=100)
-    #       outputs =  processor.batch_decode(generate_ids[:, inputs["input_ids"].shape[1]:], skip_special_tokens=True)
+            for i, row in test_data.iterrows():
 
-        predictions = []
-        translate_table = dict.fromkeys(map(ord, '\n[]\' '), None)
-        for item in outputs:
-            item = item.translate(translate_table)
-            predictions.append(item.split(","))
-        print(predictions)
-        id_predictions = []
-        n_labels = len(labels)
-        label2id = MulTweEmoDataset.get_label2id()
-        for item in predictions:
-            pred = [0] * n_labels
-            for label in item:
-                if label in labels:
-                    pred[label2id[label]] = 1
-            id_predictions.append(pred)
-
-        id_predictions = np.array(id_predictions)
-        with open(args.output, "wb") as f:
-            np.save(f, id_predictions)
-
-    else:
-        prompt_format = lambda text, emotion: f"The image is paired with this text: \"{text}\". When looking at both image and text, is the emotion evoked \"{emotion}\"? Answer with Yes or no."
-        prompts = []
-
-        for i, row in test_data.iterrows():
-            prompt_sub_list = []
-            for emotion in labels:
                 conversation = [
                     {
                         "role": "user",
                         "content": [
                             {"type": "image"},
-                            {"type": "text", "text": prompt_format(row["tweet"], emotion)},
+                            {"type": "text", "text": prompt_format(row["tweet"])},
                             ],
                     },
                 ]
-                prompt_sub_list.append(processor.apply_chat_template(conversation, add_generation_prompt=True))
-            prompts.append(prompt_sub_list)
-        processed_images= []
-        for img in test_data["img_path"]:
-            if isinstance(img, str):
-                if img.startswith('http'):
-                    response = requests.get(img)
-                    image = Image.open(BytesIO(response.content)).convert('RGB')
+                prompt = processor.apply_chat_template(conversation, add_generation_prompt=True)
+                img = row["img_path"]
+                if isinstance(img, str):
+                    if img.startswith('http'):
+                        response = requests.get(img)
+                        img = Image.open(BytesIO(response.content)).convert('RGB')
+                    else:
+                        img = Image.open(img).convert('RGB')
+                elif isinstance(img, Image.Image):
+                    img = img.convert('RGB')
                 else:
-                    image = Image.open(img).convert('RGB')
-            elif isinstance(img, Image.Image):
-                image = img.convert('RGB')
-            else:
-                raise ValueError("Unsupported image format")
-            processed_images.append(image)
-        inputs = []
-        for sub_prompt in prompts:
-            input_sub_list = []
-            for i in range(len(labels)):
-                input_sub_list.append(processor(images=processed_images[i], text=prompts[i], return_tensors="pt", padding=True).to(device))
-            inputs.append(input_sub_list)
-        predictions = np.zeros((len(inputs), len(labels)))
-        pattern = re.compile("yes", re.I)
-        with torch.no_grad():
-            for i in range(len(inputs)):
-                for j in range(len(labels)):
-                    generate_ids = model.generate(**(inputs[i][j]))
-                    output = processor.decode(generate_ids[0, inputs[i][j]["input_ids"].shape[1]:], skip_special_tokens=True)
-                    if pattern.match(output):
+                    raise ValueError("Unsupported image format")
+
+                inputs = processor(images=img, text=prompt, return_tensors="pt", padding=True).to(device)
+
+                generate_ids = model.generate(**(inputs))
+                outputs.append(processor.decode(generate_ids[0, inputs["input_ids"].shape[1]:], skip_special_tokens=True))
+
+        model_predictions = []
+        translate_table = dict.fromkeys(map(ord, '\n[]\' '), None)
+        for item in outputs:
+            item = item.translate(translate_table)
+            predictions.append(item.split(","))        
+
+        n_labels = len(labels)
+        label2id = MulTweEmoDataset.get_label2id()
+        for i in range(len(model_predictions)):
+            for label in item:
+                if label in labels:
+                    predictions[i][label2id[label]] = 1
+
+    else:
+        prompt_format = lambda text, emotion: f"The image is paired with this text: \"{text}\". When looking at both image and text, is the emotion evoked \"{emotion}\"? Answer with Yes or no."
+
+        predictions = np.zeros((len(test_data), len(labels)))
+        pos_pattern = re.compile("yes", re.I)
+        neg_pattern = re.compile("no", re.I)
+        error_counter = 0
+        with torch.no_grad():    
+            for i, row in test_data.iterrows():
+                img = row["img_path"]
+                if isinstance(img, str):
+                    if img.startswith('http'):
+                        response = requests.get(img)
+                        img = Image.open(BytesIO(response.content)).convert('RGB')
+                    else:
+                        img = Image.open(img).convert('RGB')
+                elif isinstance(img, Image.Image):
+                    img = img.convert('RGB')
+                else:
+                    raise ValueError("Unsupported image format")
+                
+                for j, emotion in enumerate(labels):
+                    conversation = [
+                        {
+                            "role": "user",
+                            "content": [
+                                {"type": "image"},
+                                {"type": "text", "text": prompt_format(row["tweet"], emotion)},
+                                ],
+                        },
+                    ]
+                    prompt = processor.apply_chat_template(conversation, add_generation_prompt=True)
+                    inputs = processor(images=img, text=prompt, return_tensors="pt", padding=True).to(device)
+                    generate_ids = model.generate(**(inputs))
+                    output = processor.decode(generate_ids[0, inputs["input_ids"].shape[1]:], skip_special_tokens=True)
+                    if pos_pattern.match(output):
                         predictions[i][j] = 1
-        
-        with open(args.output, "wb") as f:
-            np.save(f, predictions)
+                    elif not neg_pattern.match(output):
+                        error_counter += 1
+        print(f"The model gave non valid outputs {error_counter} times")
+                
+    with open(args.output, "wb") as f:
+        np.save(f, predictions)
